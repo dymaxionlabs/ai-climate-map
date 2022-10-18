6e8aaf33f8bdd28ac79e0cffbf22fcb3a025a67c
Damián Silvani <munshkr@gmail.com>
2022-10-18 11:10:50 -0300

    feat: Use bing maps basemap; try using custom terrain provider as well (disabled for now)

--- components/Cesium.js
+++ components/Cesium.js
@@ -1,15 +1,27 @@
-import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
-import { Cartesian3, createWorldTerrain } from "cesium";
+import { Viewer, Entity, PointGraphics, EntityDescription, CameraFlyTo, ImageryLayer, } from "resium";
+import { Cartesian3, createWorldTerrain, CesiumTerrainProvider, BingMapsImageryProvider } from "cesium";
 
-const terrainProvider = createWorldTerrain();
-const tegucigalpa = Cartesian3.fromDegrees(14.065049, -87.1715002, 100);
+// const terrainProvider = new CesiumTerrainProvider({
+//   url: "http://localhost:8000/tilesets/tegu_global_12pt5"
+// });
+const bingProvider = new BingMapsImageryProvider({
+  url: "https://dev.virtualearth.net",
+  key: process.env.NEXT_PUBLIC_BINGMAPS_KEY
+})
+
+const tegucigalpa = Cartesian3.fromDegrees(-87.1715002, 14.065049, 1000);
 
 export default function Cesium() {
   return (
-    <Viewer full timeline={false} animation={false} terrainProvider={terrainProvider}>
-      <Entity position={tegucigalpa} name="Tegucigalpa, Honduras">
-        <PointGraphics pixelSize={10} />
-      </Entity>
+    <Viewer
+      full
+      timeline={false}
+      animation={false}
+      shadows
+    // terrainProvider={terrainProvider}
+    >
+      <ImageryLayer imageryProvider={bingProvider} />
+      <CameraFlyTo destination={tegucigalpa} />
     </Viewer>
   )
 }
