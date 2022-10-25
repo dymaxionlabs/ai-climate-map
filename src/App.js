7896a821842dc2ae7b62ee115f4263f9a8b22767
Damián Silvani <munshkr@gmail.com>
2022-10-25 10:52:43 -0300

    feat: Basic location selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import { Color, BingMapsImageryProvider, UrlTemplateImageryProvider, Cartesian3, CesiumTerrainProvider, ShadowMode } from "cesium";
 import { CameraFlyTo, ImageryLayer, Viewer } from "resium";
+import { useState } from 'react'
+import './App.css';
+
+const locations = [
+  {
+    name: "Tegucigalpa, Honduras",
+    // center: Cartesian3.fromDegrees(-87.1715002, 14.065049, 15000),
+    center: Cartesian3.fromDegrees(-87.2399923, 14.0839129, 15000),
+    layers: [
+      { path: "/l2/tegu/flood_TEGU_low_moderado_smooth_160_30_clipbythr10.tif" }
+    ],
+  },
+  {
+    name: "Valle de Sula, Honduras",
+    center: Cartesian3.fromDegrees(-87.9242207, 15.4516219, 15000),
+    layers: [
+
+    ],
+  },
+]
+
 
 const terrainTilesetName = "alos30_honduras";
 const terrainProvider = new CesiumTerrainProvider({
@@ -16,9 +37,19 @@ const buildTitilerProvider = (cogPath) => new UrlTemplateImageryProvider({
   url: `${process.env.REACT_APP_TITILER_URL}/cog/tiles/{z}/{x}/{y}.png?url=${encodeURIComponent(cogsUrlPrefix + cogPath)}&colormap_name=ylorrd&resampling_method=bilinear`,
 });
 
-const tegucigalpa = Cartesian3.fromDegrees(-87.1715002, 14.065049, 15000);
+const LocationSelector = ({ items, value, onChange }) => (
+  <div className="location-selector">
+    <select onChange={(e) => onChange(e.target.value)}>
+      {items.map((item, i) => (
+        <option key={i} value={i} selected={value === i}>{item.name}</option>
+      ))}
+    </select>
+  </div>
+)
 
 export default function Cesium() {
+  const [currentLocation, setCurrentLocation] = useState(locations[0]);
+
   return (
     <Viewer
       timeline={false}
@@ -28,13 +59,14 @@ export default function Cesium() {
       terrainShadows={ShadowMode.CAST_ONLY}
       terrainProvider={terrainProvider}
     >
-      <ImageryLayer imageryProvider={bingProvider} />
+      <LocationSelector items={locations} onChange={i => setCurrentLocation(locations[i])} />
+      {/* <ImageryLayer imageryProvider={bingProvider} />
       <ImageryLayer
         colorToAlpha={new Color(1, 1, 0.7, 1)}
         colorToAlphaThreshold={0.075}
-        imageryProvider={buildTitilerProvider("/l2/tegu/flood_TEGU_low_moderado_smooth_160_30_clipbythr10.tif")}
-      />
-      <CameraFlyTo destination={tegucigalpa} />
+        imageryProvider={buildTitilerProvider(locations[0].layers[0].path)}
+      /> */}
+      <CameraFlyTo destination={currentLocation.center} />
     </Viewer>
   )
 }
