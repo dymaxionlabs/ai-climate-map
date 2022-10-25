3ede00390486c555eee53b299d861340c773f970
Damián Silvani <munshkr@gmail.com>
2022-10-25 10:10:35 -0300

    feat: Add COG layer using TiTiler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { BingMapsImageryProvider, Cartesian3, CesiumTerrainProvider, ShadowMode } from "cesium";
+import { Color, BingMapsImageryProvider, UrlTemplateImageryProvider, Cartesian3, CesiumTerrainProvider, ShadowMode } from "cesium";
 import { CameraFlyTo, ImageryLayer, Viewer } from "resium";
 
+const terrainTilesetName = "alos30_honduras";
 const terrainProvider = new CesiumTerrainProvider({
-  url: `${process.env.REACT_APP_CESIUMTS_URL}/alos30_honduras`
+  url: `${process.env.REACT_APP_CESIUMTS_URL}/${terrainTilesetName}`
 });
 
 const bingProvider = new BingMapsImageryProvider({
   url: "https://dev.virtualearth.net",
   key: process.env.REACT_APP_BINGMAPS_KEY
 })
+
+const buildTitilerProvider = (cogUrl) => new UrlTemplateImageryProvider({
+  url: `${process.env.REACT_APP_TITILER_URL}/cog/tiles/{z}/{x}/{y}.png?url=${encodeURIComponent(cogUrl)}&colormap_name=ylorrd&resampling_method=bilinear`,
+});
 
 const tegucigalpa = Cartesian3.fromDegrees(-87.1715002, 14.065049, 15000);
 
@@ -23,6 +28,11 @@ export default function Cesium() {
       terrainProvider={terrainProvider}
     >
       <ImageryLayer imageryProvider={bingProvider} />
+      <ImageryLayer
+        colorToAlpha={new Color(1, 1, 0.7, 1)}
+        colorToAlphaThreshold={0.075}
+        imageryProvider={buildTitilerProvider("s3://aiclimate-raster-cogs/l2/tegu/flood_TEGU_low_moderado_smooth_160_30_clipbythr10.tif")}
+      />
       <CameraFlyTo destination={tegucigalpa} />
     </Viewer>
   )
