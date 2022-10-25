38d8f2daabda6ed666817a0f6fea6a1712457277
Damián Silvani <munshkr@gmail.com>
2022-10-25 11:04:25 -0300

    feat: Better styles and content for LocationSelector

--- src/App.js
+++ src/App.js
@@ -39,6 +39,9 @@ const buildTitilerProvider = (cogPath) => new UrlTemplateImageryProvider({
 
 const LocationSelector = ({ items, value, onChange }) => (
   <div className="location-selector">
+    <span>
+      Select a known location to explore:
+    </span>
     <select onChange={(e) => onChange(e.target.value)}>
       {items.map((item, i) => (
         <option key={i} value={i} selected={value === i}>{item.name}</option>
