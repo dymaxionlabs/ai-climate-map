08611c8bab532a74149641327a6f6a6e5d66aec2
Damián Silvani <munshkr@gmail.com>
2022-11-24 22:29:26 -0300

    feat: Reorder L1-L4 groups

--- src/data.js
+++ src/data.js
@@ -13,10 +13,10 @@ export const locations = [
     // center: Cartesian3.fromDegrees(-87.1715002, 14.065049, 15000),
     center: Cartesian3.fromDegrees(-87.2399923, 14.0839129, 15000),
     groups: [
-      { id: "l1", name: "Level 1: Informal Settlements" },
-      { id: "l2", name: "Level 2: Flooded Areas" },
-      { id: "l3", name: "Level 3: Landslides" },
       { id: "l4", name: "Level 4: Land Value" },
+      { id: "l3", name: "Level 3: Landslides" },
+      { id: "l2", name: "Level 2: Flooded Areas" },
+      { id: "l1", name: "Level 1: Informal Settlements" },
     ],
     legends: [
       {
